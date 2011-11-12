8f99dc99e993b7f034cef77af29020c68363915c
nn0cl <nn0cl@sumstates.net>
2011-11-12 17:12:18 +0900

    initialized

--- seed/main.cpp
+++ seed/main.cpp
@@ -1,18 +1,42 @@
 //
-//  main.cpp
-//  seed
+//  File.cpp
+//  seeds
 //
-//  Created by Suguru Yonemura on 11/11/12.
-//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//  Created by Suguru Yonemura on 11/10/30.
+//  Copyright (c) 2011年 sumstates.net. All rights reserved.
 //
 
 #include <iostream>
+#include "SeedBinaryTest.h"
+#include "DataTest.h"
+#include "FileManagerTest.h"
 
-int main (int argc, const char * argv[])
+int main(int argc,char** argv)
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    fileManagerTest();
+    
+    int tType = 1;
+    int r = 0;
+    
+    SeedBinaryTest* sbt =new SeedBinaryTest();
+    DataTest* dt = new DataTest();
+    std::cout<<"This is a test Data"<<std::endl;
+    
+    if(tType== 0 ){
+        r = dt->utData();
+        if(r!=0){
+            std::cout<<"ERROR " << std::endl;
+        }
+    }else if(tType==1){
+        r = sbt->utSeed();
+        if(r!=0){
+            std::cout<<"ERROR" << std::endl;
+        }
+        
+    }
+    
+    std::cout<<"End"<<std::endl;
+    
     return 0;
 }
-
