a38b8a230c0418190d6963cabb4e626e4091590a
nn0cl <nn0cl@sumstates.net>
2011-11-12 17:03:00 +0900

    Initial Commit

---
+++ seed/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  seed
+//
+//  Created by Suguru Yonemura on 11/11/12.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
