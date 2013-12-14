73f8a2d05e789f5b7fefcd3feb11f3d0cd1b9f87
nn0cl <nn0cl@dstechnology.co.jp>
2013-12-14 15:18:47 +0900

    Initial Commit

---
+++ seeds/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  seeds
+//
+//  Created by nn0cl on 2013/12/14.
+//  Copyright (c) 2013年 nn0cl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
