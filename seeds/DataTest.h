8c71b6b3952d548e0e9b81b4f80870946c9841f0
nn0cl <nn0cl@dstechnology.co.jp>
2017-12-28 22:22:20 +0900

    first commit

---
+++ seeds/DataTest.h
@@ -0,0 +1,20 @@
+//
+//  DataTest.h
+//  seeds
+//
+//  Created by Suguru Yonemura on 11/10/30.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef seeds_DataTest_h
+#define seeds_DataTest_h
+
+#include <iostream>
+#include "Data.h"
+class DataTest{
+public:
+int utData();
+    
+};
+
+#endif
---
+++ seeds/Manager.cpp
@@ -0,0 +1,9 @@
+//
+//  Manager.cpp
+//  seed_base
+//
+//  Created by Yonemura Suguru on 11/10/25.
+//  Copyright 2011年 sumstates.net. All rights reserved.
+//
+
+//#include <iostream>
